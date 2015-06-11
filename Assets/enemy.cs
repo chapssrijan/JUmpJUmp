b64ebfba13a62ce6168bff773d412f2f60f2240d
chapssrijan <srijan.chapagain@cou.fi>
2015-06-11 13:57:43 +0300

    asd

---
+++ Assets/enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy : MonoBehaviour {
+
+	bool timer=false;
+
+	Rigidbody2D rb;
+	public float speed=5;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x >=-10.33252) {
+
+			timer = false;
+		}
+		if (transform.position.x <= -12.6) {
+
+			timer = true;
+		}
+
+		if (timer) {
+			rb.AddForce (transform.right * speed);
+		} else {
+			rb.AddForce(transform.right * -speed);
+		}
+
+	}
+}
