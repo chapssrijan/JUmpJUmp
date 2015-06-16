707b6f667ce075d6244d8fd1670c9a95adfdd500
chapssrijan <srijan.chapagain@cou.fi>
2015-06-16 14:09:01 +0300

    xasdd updated

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -4,29 +4,43 @@ using System.Collections;
 public class enemy : MonoBehaviour {
 
 	bool timer=false;
+	float scalex;
 
 	Rigidbody2D rb;
 	public float speed=5;
 	// Use this for initialization
 	void Start () {
+		scalex =  transform.localScale.x;
+	
 		rb = GetComponent<Rigidbody2D> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (transform.position.x >=-10.5) {
 
-			timer = false;
-		}
-		if (transform.position.x <= -12.6) {
+		if (transform.position.x >=-10.8) {
 
 			timer = true;
 		}
 
+		if (transform.position.x <= -13) {
+
+			timer = false;
+		}
+	
+
 		if (timer) {
 			rb.AddForce (transform.right * speed);
+			{
+			if (transform.localScale.x < 0)
+				transform.localScale = new Vector3 (scalex, transform.localScale.y, transform.localScale.z);
+			}
+
 		} else {
-			rb.AddForce(transform.right * -speed);
+			rb.AddForce (transform.right * -speed);
+			if (transform.localScale.x > 0) {
+				transform.localScale = new Vector3 (-scalex, transform.localScale.y, transform.localScale.z);
+			}
 		}
 
 	}
